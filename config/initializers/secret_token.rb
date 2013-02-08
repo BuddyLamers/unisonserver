8e5418add01ca6cbf1e99db0ff09b1027abb0f30
Kyle Warren <qarren@gmail.com>
2013-02-08 15:08:24 -0500

    added all rails files/folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unisonserver::Application.config.secret_token = '6d977980e6d1041a0de1252adebd624423312e99f5fa2e902cd3f4bd445c084e857f810852857fb99dae363db1fee9d90b17320d704a89b623357d4fc2dd1bb0'
