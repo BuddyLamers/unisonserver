0d47ac6a6febb028c8d3819ccc4570e965c34fdb
Kyle Warren <qarren@gmail.com>
2013-03-19 11:01:56 -0400

    removed people relationship arrays

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,8 +3,8 @@ class Person
   include Mongoid::Timestamps
   include Mongoid::Realization
 
-  has_and_belongs_to_many :breaches
-  has_and_belongs_to_many :sessions
+  has_many :breaches
+  has_many :sessions
   has_many :contributions
 
   field :fname, type: String
