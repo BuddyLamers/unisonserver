c808e0d55bd00d103228ce4273ff458c2d7c7aa6
Kyle Warren <qarren@gmail.com>
2013-05-22 13:49:43 -0400

    fixed import codes script for code types

--- script/import_codes.rb
+++ script/import_codes.rb
@@ -2,6 +2,7 @@ require "csv"
 
 topics = {}
 subjects = {}
+code_types = {}
 
 CSV.foreach(Rails.root.join('db', 'subject_codes.csv'), {
     headers: :first_row
@@ -17,6 +18,9 @@ CSV.foreach(Rails.root.join('db', 'subject_codes.csv'), {
     # this is topic
     if info.length < 2
       topics[attrs[:text]] = attrs[:name]
+      code_type = CodeType.where(key: attrs[:text]).first
+      code_type = CodeType.create(key: attrs[:text], name: attrs[:name]) unless code_type
+      code_types[attrs[:text]] = code_type
     else
       topic_code = attrs[:name][/[A-Z]+/]
 
@@ -33,7 +37,22 @@ CSV.foreach(Rails.root.join('db', 'subject_codes.csv'), {
 
       attrs[:subject] = subjects[attrs[:subject]]
 
-      code = Code.create(attrs)
+      if !code_types[topic_code]
+        code_type = CodeType.where(key: topic_code).first
+        code_type = CodeType.create(key: topic_code, name: "No Name") unless code_type
+        code_types[topic_code] = code_type
+      end
+
+      attrs[:code_type] = code_types[topic_code]
+
+      code = Code.where(name: attrs[:name]).first
+
+      if code
+        code.update_attributes(attrs)
+      else
+        code = Code.create(attrs)
+      end
+
       puts code.attributes
       puts code.errors.full_messages
     end
